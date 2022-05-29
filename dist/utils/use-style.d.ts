2270ddb868cc2f46fe26fac5747b700a04ed0ff8
mocan <1318219282@qq.com>
2022-05-29 14:40:04 +0800

    #fix-去除颜色参数处理

--- dist/utils/use-style.d.ts
+++ dist/utils/use-style.d.ts
@@ -33,25 +33,4 @@ declare type styleFormatProp = {
 };
 export declare const getStyleFormat: (style: styleFormatProp[]) => {};
 export declare const getClassFomat: (cl: string | Array<string>) => string;
-export interface IColorObj {
-    r: number;
-    g: number;
-    b: number;
-    a?: number;
-}
-export declare const toHex: (n: number) => string;
-export declare const toHexString: (colorObj: IColorObj) => string;
-export declare const toRgbString: (colorObj: IColorObj) => string;
-export declare const toRgbaString: (colorObj: IColorObj, n?: number) => string;
-export declare const parseHexColor: (color: string) => IColorObj;
-export declare const parseRgbaColor: (color: string) => IColorObj;
-export declare const parseColorString: (color: string) => IColorObj;
-export declare const getColorInfo: (color: string) => {
-    hex: string;
-    rgba: string;
-    rgb: string;
-    rgbaObj: IColorObj;
-};
-export declare const hexToRgba: (hex: string) => string;
-export declare const rgbaToHex: (rgba: string) => string;
 export {};
